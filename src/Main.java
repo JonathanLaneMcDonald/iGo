f0aaa05c1c52c8b555f15c74d1f5776c6af0d799
Lane McDonald <Jonathan.Lane.McDonald@gmail.com>
2021-01-11 12:07:57 +1100

    reintroduced the random sampler test for speed and correctness tests

--- src/Main.java
+++ src/Main.java
@@ -53,6 +53,8 @@ afterward:
 	{
 		//simulationSpeedTest("res\\compliant kgs games",19);
 
+		//randomSamplerTest(10000, 9);
+
 		// some have included dirichlet noise to the PUCT function, too, so maybe add a term for that
 		// https://stats.stackexchange.com/questions/322831/purpose-of-dirichlet-noise-in-the-alphazero-paper
 		mctsSelfPlayTest(7, 100, 1);
@@ -96,6 +98,63 @@ afterward:
 			}
 		}
 		return moveNumber;
+	}
+
+	private static void randomSamplerTest(int gamesToPlay, int edgeLength)
+	{
+		int errors = 0;
+		int gamesPlayed = 0;
+		int movesPlayed = 0;
+		int gamesWithErrors = 0;
+		long startTime = System.nanoTime();
+		while(gamesPlayed < gamesToPlay)
+		{
+			int player = 1;
+			var game = new iGo(edgeLength);
+			boolean gameContainsErrors = false;
+
+			var random = new Random();
+			int consecutivePasses = 0;
+			while(consecutivePasses < 2)
+			{
+				//var legalMoves = game.getMovesLegalForBothPlayers();
+				var sensibleMoves = game.getSensibleMovesForPlayer(player);
+				if(sensibleMoves.isEmpty())
+					consecutivePasses ++;
+				else
+				{
+					movesPlayed ++;
+					consecutivePasses = 0;
+
+					var mv = sensibleMoves.get(random.nextInt(sensibleMoves.size()));
+					var success = game.placeStone(mv, player);
+					if(!success)
+					{
+						errors ++;
+						gameContainsErrors = true;
+					}
+					player = -player;
+				}
+			}
+
+			//System.out.println(game.getSimpleTerminalScore());
+			//game.display(new HashSet<>());
+			//game.displayLiberties(new HashSet<>());
+
+			if(gameContainsErrors)
+			{
+				gamesWithErrors ++;
+			}
+
+			gamesPlayed ++;
+			if(gamesPlayed % 1000 == 0)
+			{
+				var elapsedTime = System.nanoTime() - startTime;
+				var timePerGame = elapsedTime / gamesPlayed;
+				var timePerMove = elapsedTime / movesPlayed;
+				System.out.println("Moves: "+movesPlayed+" Games: "+gamesPlayed+" ("+movesPlayed/gamesPlayed+"mpg) Time/(m,g): ("+timePerMove+","+timePerGame+") Errors/(m,g): ("+errors+","+gamesWithErrors+")");
+			}
+		}
 	}
 
 	private static void simulationSpeedTest(String sgfFileLocation, int edgeLength)
